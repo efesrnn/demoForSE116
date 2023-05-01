206479cc198732e969c54967a65c74f780554d59
Efe Serin <efe.serin999@gmail.com>
2023-05-01 17:31:35 +0300

    filePath added

--- Misti/src/PointFileReader.java
+++ Misti/src/PointFileReader.java
@@ -2,16 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+
 public class PointFileReader {
     private HashMap<String, Integer> pointValues;
 
     public PointFileReader(String filename) {
         pointValues = new HashMap<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\efese\\OneDrive\\Masaüstü\\points.txt"));
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(","); // Change the delimiter if needed
+                String[] parts = line.split("\\s+"); // Change the delimiter to whitespace (or another delimiter if needed)
                 String key = parts[0].trim();
                 int value = Integer.parseInt(parts[1].trim());
                 pointValues.put(key, value);
@@ -21,9 +22,9 @@ public class PointFileReader {
             e.printStackTrace();
         }
     }
+
     public int getPointValue(Card card) {
         String identifier = card.getSuit() + card.getNumber(); // Change this to the desired identifier
         return pointValues.getOrDefault(identifier, 0); // Returns 0 if the identifier is not in the HashMap
     }
-
 }
