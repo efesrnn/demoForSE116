fa3ade0cea67a1d661892053f7d1d44d28f07d40
Gokberk Gok <gokscloud@gmail.com>
2023-05-06 19:34:02 +0300

    some expert bugs are fixed

--- src/Expert.java
+++ src/Expert.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class Expert extends Players{
         private int playedCard;
-
-        private ArrayList<Card> jokers= new ArrayList<Card>();
         private ArrayList<Character> leftCards= new ArrayList<Character>();
         private ArrayList<Integer> cardsOnHandNumbers = new ArrayList<>();
 
@@ -14,10 +12,6 @@ public class Expert extends Players{
             Random rd = new Random();
             int random= rd.nextInt(cardsOnHand.size());
             int counter=0;
-            jokers.add(new Card("♣","J"));
-            jokers.add(new Card("♠","J"));
-            jokers.add(new Card("♥","J"));
-            jokers.add(new Card("♦","J"));
             for(int i=0;i<cardsOnHand.size()+counter;i++) {//burada elimizdeki herhangi bir kart üstteki kartla uyuşuyormu diye bakıyoruz.
                 if (i< cardsOnHand.size()&&cardsOnArea.get(cardsOnArea.size() - 1).toString().indexOf(1) ==cardsOnHand.get(i).toString().indexOf(1)){
                     playedCard=i;
@@ -32,7 +26,7 @@ public class Expert extends Players{
                 }
 
                 if(i>cardsOnHand.size()-2){counter++;}//eğer öncelikli kart bulunamadıysa joker için arttırıyoruz counterı
-                if(i>cardsOnHand.size()-2&&cardsOnHand.get(i-3)==jokers.get(i-3)) {
+                if(i>cardsOnHand.size()-2&&cardsOnHand.get(i-3).getNumber().equals("J")) {
                     playedCard=i-3;
                     cardsOnHand.remove(i-3);
                     cardNum--;
@@ -40,41 +34,43 @@ public class Expert extends Players{
                     gatheredCards.addAll(cardsOnArea);
                     break;
                 } else if(counter==cardsOnHand.size()) {//asıl olay burada başlıyor
-                    leftCards.clear();
                     cardsOnHandNumbers.clear();
-
-                    for(int j=0;j<super.getDeck().allCards.size();j++){
-                        leftCards.add(super.getDeck().allCards.get(i).toString().charAt(1));
-                    }
                     for(int j=0;j<13;j++){
                         leftCardsNumbers[j]=0;
                     }
-                    for(int j=0;j<leftCards.size();j++){
-                        if (leftCards.get(j).equals("K")){leftCardsNumbers[12]++;}
-                        else if (leftCards.get(j).equals("Q")){leftCardsNumbers[11]++;}
-                        else if (leftCards.get(j).equals("J")){leftCardsNumbers[10]++;}
-                        else if (leftCards.get(j).equals("A")){leftCardsNumbers[0]++;}
+                    for(int j=0;j<super.getDeck().allCards.size();j++){
+                        if (super.getDeck().allCards.get(j).getNumber().equals("K")){leftCardsNumbers[12]++;}
+                        else if (super.getDeck().allCards.get(j).getNumber().equals("Q")){leftCardsNumbers[11]++;}
+                        else if (super.getDeck().allCards.get(j).getNumber().equals("J")){leftCardsNumbers[10]++;}
+                        else if (super.getDeck().allCards.get(j).getNumber().equals("A")){leftCardsNumbers[0]++;}
                         else{
-                            leftCardsNumbers[Character.getNumericValue(leftCards.get(j))-1]++;
+                            leftCardsNumbers[Integer.parseInt(super.getDeck().allCards.get(j).getNumber())-1]++;
                         }
                     }
                     for(int j=0;j<cardsOnHand.size();j++) {
-                        cardsOnHandNumbers.add(leftCardsNumbers[Character.getNumericValue(cardsOnHand.get(j).toString().charAt(1))]);
+                        if (cardsOnHand.get(j).getNumber().equals("K")){cardsOnHandNumbers.add(leftCardsNumbers[12]);}
+                        else if (cardsOnHand.get(j).getNumber().equals("Q")){cardsOnHandNumbers.add(leftCardsNumbers[11]);}
+                        else if (cardsOnHand.get(j).getNumber().equals("J")){cardsOnHandNumbers.add(leftCardsNumbers[10]);}
+                        else if (cardsOnHand.get(j).getNumber().equals("A")){cardsOnHandNumbers.add(leftCardsNumbers[0]);}
+                        else {
+                            cardsOnHandNumbers.add(leftCardsNumbers[Character.getNumericValue(cardsOnHand.get(j).toString().charAt(1))]);
+                        }
                     }
-                    Collections.sort(cardsOnHandNumbers,Collections.reverseOrder());
+                    Collections.sort(cardsOnHandNumbers,Collections.reverseOrder());//dağıtılmayan kartların sayılarını çoktan aza doğru sıralar
+
                     for(int j=0;j<cardsOnHand.size();j++){
-                        if (Character.getNumericValue(cardsOnHand.get(j).toString().charAt(1))==cardsOnHandNumbers.get(0)){
+                        int number;
+                        if(cardsOnHand.get(j).getNumber().equals("K")){number=12;}
+                        else if (cardsOnHand.get(j).getNumber().equals("Q")){number=11;}
+                        else if(cardsOnHand.get(j).getNumber().equals("J")){number=10;}
+                        else {number=Integer.parseInt(cardsOnHand.get(j).getNumber());}
+                        if (leftCardsNumbers[number]==cardsOnHandNumbers.get(0)){
                             cardsOnArea.add(cardsOnHand.get(j));
                             cardsOnHand.remove(j);
                             cardNum--;
-
+                            break;
                         }
                     }
-
-
-                    cardsOnHand.remove(0);//düzenle burayı
-                    cardNum--;
-                    break;
                 }
             }
         }
